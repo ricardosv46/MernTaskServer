c61b09b399f52fed072710ff9c58306ccdb2c7b4
RICARDO SOLIS <ricardosv46@gmail.com>
2022-02-19 00:11:17 -0500

    update cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ conectarDB()
 const whitelist = [
   'http://localhost:3000',
   'https://mern-stack-proyectos.vercel.app',
-  'https://mern-proyect.netlify.app/'
+  'https://mern-proyect.netlify.app'
 ]
 const options = {
   origin: (origin, callback) => {
