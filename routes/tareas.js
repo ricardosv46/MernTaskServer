36e8e8e445cf2209bd2ac0624835f96b497b754f
RICARDO SOLIS <ricardosv46@gmail.com>
2022-02-18 23:51:20 -0500

    fix

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const TareaController = require('../Controllers/TareaController')
+const TareaController = require('../Controllers/tareaController')
 const auth = require('../middleware/auth')
 const { check } = require('express-validator')
 
